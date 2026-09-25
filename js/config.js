6619f5be633969039c355b94f1d10b09fdf227d2
adiva-jain <jainadiv@msu.edu>
2026-09-24 21:51:56 -0400

    store api urls and color constants

---
+++ js/config.js
@@ -0,0 +1,12 @@
+export const ISS_URL = "https://api.wheretheiss.at/v1/satellites/25544";
+export const LAND_URL = "https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_110m_land.geojson";
+export const POLL_MS = 5000;
+export const TRAIL_LENGTH = 300;
+
+export const COLORS = {
+    ocean: "#0e2a47",
+    land: "#2f5d50",
+    grid: "#1a3d5f",
+    iss: "#ffd166",
+    issGlow: "rgba(255, 209, 102, 0.2)",
+};
